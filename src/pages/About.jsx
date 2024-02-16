1a7a04201485a52bb5879188841a60184404ba1f
victorreverter <victormanuel.cuba@gmail.com>
2024-02-16 06:11:02 +0100

    update base code 4.1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,9 +14,10 @@ const About = () => {
       <div className='top-container'>
 
         <div className="subtitle-container">
-          <h4 className='subtitle'>Full stack designer with 9 years of experience focused in the creation of graphic, web, and identity products with that mojo of make things as better as possible and good to be used.</h4>
+          <h4 className='description-about'>Full stack designer with 9 years of experience focused in the creation of graphic, web, and identity products with that mojo of make things as better as possible and good to be used.</h4>
+          <h4 className='description-about'>Currently working as Web Design Manager at Telecoming, and when I am not designing I am running Half Marathons, reading, playing chess and more.</h4>
 
-          <Link to={"#"} className='btn-item' href="" target="_blank">
+          <Link to={"#"} className='cv-link-btn' href="" target="_blank">
             <div className="container-text">
               <span>My Resume</span>
             </div>
@@ -32,17 +33,18 @@ const About = () => {
 
       <div className='top-container'>
 
-        <div className="subtitle-container">
-          <h4 className='subtitle'>Growing up in Havana, Cuba, where life is full of colorful vibes and lively rhythms, I moved to Spain when I was quite young. I went to a Design School there, and it was in Web Design that I found my true passion – the cool mix of design, art, and the technical side of things.</h4>
-          <h4 className='subtitle'>From the bustling streets of Havana to the modern scenes of Spain, my experiences have made my view of the world pretty diverse. It's kind of like how Brasília, the modern capital of Brazil, has a mix of influences.</h4>
-          <h4 className='subtitle'>I got into design because, like in the example, I was drawn to it at a young age. Think of it like when they talk about the beige computer sparking an interest in technology. Even though I graduated with top marks in Design, my real excitement comes from the way great design, especially like what you see in Apple products, can totally change how we feel.</h4>
-          <h4 className='subtitle'>As I thought about the impact of awesome designs, I wondered if I could make something that leaves a mark on people too.</h4>
-          <h4 className='subtitle'>Nowadays, using my design skills from different areas, I'm on a mission to make things that go beyond just being useful. I want my designs to make people feel something special, turning everyday moments into something inspiring.</h4>
-          <h4 className='subtitle'></h4>
-        </div>
+        {/* <div className="paragraph-container">
+          <p className='paragraph'>I grew up in Havana, Cuba, where life is full of colorful vibes and lively rhythms. Then I moved to Spain when I was quite young. I went to a Design School there, and it was in Web Design that I found my true passion – the cool mix of design, art, and the technical side of things.</p>
+          <p className='paragraph'>From the bustling streets of Havana to the modern scenes of Spain, my experiences have made my view of the world pretty diverse. It's kind of like how Brasília, the modern capital of Brazil, has a mix of influences.</p>
+          <p className='paragraph'>I got into design because, like in the example, I was drawn to it at a young age. Think of it like when they talk about the beige computer sparking an interest in technology. Even though I graduated with top marks in Design, my real excitement comes from the way great design, especially like what you see in Apple products, can totally change how we feel.</p>
+          <p className='paragraph'>As I thought about the impact of awesome designs, I wondered if I could make something that leaves a mark on people too.</p>
+          <p className='paragraph'>Nowadays, using my design skills from different areas, I'm on a mission to make things that go beyond just being useful. I want my designs to make people feel something special, turning everyday moments into something inspiring.</p>
+          <p className='paragraph'></p>
+        </div> */}
         
-      </div>
-      
+        <div className="skills-container">
+        </div>
+      </div>      
 
       <h5>Skills</h5>
 
@@ -94,13 +96,13 @@ const About = () => {
         <li>Learn Languages</li>
       </ul>
 
-      <h5>My Resume</h5>
+      {/* <h5>My Resume</h5> */}
 
       {/* <a className='cv-link' href="" target="_blank" rel="noopener noreferrer">
         <p>See my CV</p>
       </a> */}
 
-      <Link to={"#"} className='cv-link-btn' href="" target="_blank">
+      {/* <Link to={"#"} className='cv-link-btn' href="" target="_blank">
         <div className="container-text">
           <span>See my CV</span>
         </div>
@@ -108,7 +110,7 @@ const About = () => {
           <path d="M1,5 L11,5"></path>
           <polyline points="8 1 12 5 8 9"></polyline>
         </svg>
-      </Link>
+      </Link> */}
 
       {/* <Footer /> */}
     </div>
