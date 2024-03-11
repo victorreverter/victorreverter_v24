98dbc52620951ff5328551401a253b86e79f9005
victorreverter <victormanuel.cuba@gmail.com>
2024-03-11 05:59:14 +0100

    update base code 5.4

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from "react";
 // import './Home.css';
 import '../styles/global.css';
 import '../styles/hero-home.css';
 
 const Home = () => {
+  useEffect(() => {
+    console.log('Home component mounted');
+    
+    const HeroName = document.querySelector('#HeroName');
+    const HeroNameContent = document.querySelector('#HeroNameContent');
+    
+    setTimeout(() => {
+      HeroName.classList.add('hero-home-activated');
+    }, 50);
+
+    setTimeout(() => {
+      HeroNameContent.classList.add('hero-home-content-activated');
+    }, 700);
+
+  }, []);
+
   return (
-    <section className="hero-home">
+    <section id="HeroName" className="hero-home">
       <div className='gradient-over'></div>
 
-      <div className="hero-home-content">
+      <div id="HeroNameContent" className="hero-home-content">
         <h3>Victor Reverter</h3>
 
         <h1>Full Stack</h1>
